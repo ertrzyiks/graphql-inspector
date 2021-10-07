f2e49f92b9e99c8509523b419a0cfad9c8c85e86
ertrzyiks <mateusz.derks@gmail.com>
2021-10-07 08:13:46 +0200

    Add debug

--- packages/action/src/action.ts
+++ packages/action/src/action.ts
@@ -5,6 +5,14 @@
 import * as core from '@actions/core';
 import {run} from './run';
 
+process.on('uncaughtException', (err: Error, origin: string) => {
+  console.log(
+    `Caught exception: ${err}\n` +
+    `Exception origin: ${origin}`
+  );
+  console.log('Stack', err.stack)
+})
+
 run().catch((e) => {
   core.setFailed(e.message || e);
 });
